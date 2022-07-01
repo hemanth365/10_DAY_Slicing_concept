10042040832e38de5a43c768558c2542a7f60a36
Hemanth Krishna Nanda <107603676+hemanth365@users.noreply.github.com>
2022-07-01 14:43:18 +0530

    Add files via upload

---
+++ slicing_2.py
@@ -0,0 +1,65 @@
+# A='DICTIONARY'
+# print(A[:10])
+# print(A[1:10])
+# print(A[2:10])
+# print(A[3:10])
+# print(A[4:10])
+# print(A[5:10])
+# print(A[6:10])
+# print(A[7:10])
+# print(A[8:10])
+# print(A[9:10])
+# print(A[10:10])
+
+# B="123456789"
+# print(int(B[0])+int(B[-1]))
+# print(int(B[5])+int(B[8]))
+# print(int(B[0])+int(B[5]))
+# print(int(B[2])+int(B[-1]))
+# print(int(B[4])+int(B[6]))
+# print(int(B[1])+int(B[3]))
+
+C="COMMAND-LINE"
+print(C[1::10])
+print(C[0::2])
+print(C[2::3])
+print(C[1::4])
+print(C[5::2])
+print(C[7::1])
+print(C[0::1])
+print(C[::-1])   # reverse of a string
+print(C[::-2])   # reverse of a string
+print(C[::1])
+print(C[0::6])
+print(C[5::1])
+print(C[6:10:1])
+print(C[8:12:1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
